9435850b38ff7de797a69e12a545dfe6b1c08fd7
Pankaj P <54427553+PankHub@users.noreply.github.com>
2020-07-26 23:39:23 +0530

    Added logic for time and changed typograpy

---
+++ newtab.js
@@ -0,0 +1,13 @@
+function startTime() {
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+    document.getElementById('txt').innerHTML = strTime;
+    var t = setTimeout(startTime, 500);
+}
+window.onload = startTime;
